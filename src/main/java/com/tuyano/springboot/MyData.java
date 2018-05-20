9328b4616e99a35adf93a18d166897699c583a09
kashi <kashi@com>
2018-05-20 15:16:22 +0900

    Lombok化

--- src/main/java/com/tuyano/springboot/MyData.java
+++ src/main/java/com/tuyano/springboot/MyData.java
@@ -8,7 +8,9 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import lombok.Data;
 
+@Data
 @Entity
 @Table(name="mydata")
 public class MyData {
@@ -36,49 +38,6 @@ public class MyData {
 	public MyData() {
 		super();
 		room = new Room();
-	}
-	
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getMail() {
-		return mail;
-	}
-	public void setMail(String mail) {
-		this.mail = mail;
-	}
-
-	public Integer getAge() {
-		return age;
-	}
-	public void setAge(Integer age) {
-		this.age = age;
-	}
-
-	public String getMemo() {
-		return memo;
-	}
-	public void setMemo(String memo) {
-		this.memo = memo;
-	}
-	
-	public Room getRoom() {
-		return room;
-	}
-
-	public void setRoom(Room room) {
-		this.room = room;
 	}
 	
 }
--- src/main/java/com/tuyano/springboot/MyDataForm.java
+++ src/main/java/com/tuyano/springboot/MyDataForm.java
@@ -2,6 +2,11 @@ package com.tuyano.springboot;
 
 import java.io.Serializable;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper=true)
 public class MyDataForm extends MyData implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -46,157 +51,4 @@ public class MyDataForm extends MyData implements Serializable {
     */
     private String date1;
     
-    /**
-     * 年齢Fromを返す。
-     * @return 年齢From
-     */
-    public Integer getAgeFrom() {
-        return ageFrom;
-    }
-    
-    /**
-     * 年齢Fromを設定する。
-     * @param ageFrom 年齢From
-     */
-    public void setAgeFrom(Integer ageFrom) {
-        this.ageFrom = ageFrom;
-    }
-    
-    /**
-     * 年齢Toを返す。
-     * @return 年齢To
-     */
-    public Integer getAgeTo() {
-        return ageTo;
-    }
-    
-    /**
-     * 年齢Toを設定する。
-     * @param ageTo 年齢To
-     */
-    public void setAgeTo(Integer ageTo) {
-        this.ageTo = ageTo;
-    }
-    
-    /**
-     * チェック1を返す。
-     * @return チェック1
-     */
-    public boolean isCheck1() {
-        return check1;
-    }
-    
-    /**
-     * チェック1を設定する。
-     * @param check1 チェック1
-     */
-    public void setCheck1(boolean check1) {
-        this.check1 = check1;
-    }
-    
-    /**
-     * チェック2を返す。
-     * @return チェック2
-     */
-    public String[] getCheck2() {
-        return check2;
-    }
-    
-    /**
-     * チェック2を設定する。
-     * @param check2 チェック2
-     */
-    public void setCheck2(String[] check2) {
-        this.check2 = check2;
-    }
-    
-    /**
-     * ラジオ1を返す。
-     * @return ラジオ1
-     */
-    public String getRadio1() {
-        return radio1;
-    }
-    
-    /**
-     * ラジオ1を設定する。
-     * @param radio1 ラジオ1
-     */
-    public void setRadio1(String radio1) {
-        this.radio1 = radio1;
-    }
-    
-    /**
-     * セレクト1を返す。
-     * @return セレクト1
-     */
-    public String getSelect1() {
-        return select1;
-    }
-    
-    /**
-     * セレクト1を設定する。
-     * @param select1 セレクト1
-     */
-    public void setSelect1(String select1) {
-        this.select1 = select1;
-    }
-    
-    /**
-     * セレクト2を返す。
-     * @return セレクト2
-     */
-    public String[] getSelect2() {
-        return select2;
-    }
-    
-    /**
-     * セレクト2を設定する。
-     * @param select2 セレクト2
-     */
-    public void setSelect2(String[] select2) {
-        this.select2 = select2;
-    }
-    
-    /**
-     * 日付1を返す。
-     * @return 日付1
-     */
-    public String getDate1() {
-        return date1;
-    }
-    
-    /**
-     * 日付1を設定する。
-     * @param date1 日付1
-     */
-    public void setDate1(String date1) {
-        this.date1 = date1;
-    }
-    
-    /**
-     * オブジェクトの文字列表現を返す。
-     * @return オブジェクトの文字列表現
-     */
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(" , ");
-        sb.append("[ageFrom=").append(getAgeFrom()).append("]");
-        sb.append(" , ");
-        sb.append("[ageTo=").append(getAgeTo()).append("]");
-        sb.append(" , ");
-        sb.append("[check1=").append(isCheck1()).append("]");
-        sb.append(" , ");
-        sb.append("[check2=").append(getCheck2()).append("]");
-        sb.append(" , ");
-        sb.append("[radio1=").append(getRadio1()).append("]");
-        sb.append(" , ");
-        sb.append("[select1=").append(getSelect1()).append("]");
-        sb.append(" , ");
-        sb.append("[select2=").append(getSelect2()).append("]");
-        sb.append(" , ");
-        sb.append("[date1=").append(getDate1()).append("]");
-        return sb.toString();
-    }
-
 }
