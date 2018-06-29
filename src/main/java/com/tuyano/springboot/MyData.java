7c049f2ccafab0b6df5b8d7b853e00211faef42e
kashi <kashi@com>
2018-06-29 22:41:41 +0900

    新規登録できるようにGenerationType.IDENTITY

--- src/main/java/com/tuyano/springboot/MyData.java
+++ src/main/java/com/tuyano/springboot/MyData.java
@@ -20,7 +20,7 @@ import lombok.ToString;
 public class MyData implements Serializable{
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column
 	private Long id;
 
