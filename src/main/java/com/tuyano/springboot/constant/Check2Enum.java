95941d272c72e2e53e64226341f39aa21cf74d5a
kashi <kashi@com>
2018-06-29 22:20:58 +0900

    画面横並び

--- src/main/java/com/tuyano/springboot/constant/Check2Enum.java
+++ src/main/java/com/tuyano/springboot/constant/Check2Enum.java
@@ -7,9 +7,9 @@ import lombok.Getter;
 @Getter
 public enum Check2Enum {
 
-  チェック21("1", "チェック21表示"),
-  チェック22("2", "チェック22表示"),
-  チェック23("3", "チェック23表示");
+  チェック21("1", "チェック21"),
+  チェック22("2", "チェック22"),
+  チェック23("3", "チェック23");
 
   private final String code;
   private final String label;
