113e35c1a7e2b435c4680c4e30499bb2b422739f
kashi <kashi@com>
2018-07-01 07:14:03 +0900

    mysqlからh2にする

--- src/main/java/com/tuyano/springboot/controller/MyDataController.java
+++ src/main/java/com/tuyano/springboot/controller/MyDataController.java
@@ -165,45 +165,45 @@ public class MyDataController {
 	@PostConstruct
 	public void init(){
 		
-//		Item i1 = new Item();
-//		i1.setItemName("アイテム1");
-//		itemRepository.saveAndFlush(i1);
-//		Item i2 = new Item();
-//		i2.setItemName("アイテム2");
-//		itemRepository.saveAndFlush(i2);
-//		
-//		
-//		Room r1 = new Room();
-//		r1.setName("いっかい");
-//		r1.setItem(i1);
-//		roomRepository.saveAndFlush(r1);
-//		Room r2 = new Room();
-//		r2.setName("にかい");
-//		r2.setItem(i2);
-//		roomRepository.saveAndFlush(r2);
-//		
-//		MyData d1 = new MyData();
-//		d1.setName("tuyano");
-//		d1.setAge(123);
-//		d1.setMail("syoda@tuyano.com");
-//		d1.setMemo("090999999");
-//		d1.setRoom(r1);
-//		repository.saveAndFlush(d1);
-//		MyData d2 = new MyData();
-//		d2.setName("hanako");
-//		d2.setAge(15);
-//		d2.setMail("hanako@flower");
-//		d2.setMemo("080888888");
-//		d2.setRoom(null);
-////		d2.setRoom(r1);
-//		repository.saveAndFlush(d2);
-//		MyData d3 = new MyData();
-//		d3.setName("sachiko");
-//		d3.setAge(37);
-//		d3.setMail("sachico@happy");
-//		d3.setMemo("070777777");
-//		d3.setRoom(r2);
-//		repository.saveAndFlush(d3);
+		Item i1 = new Item();
+		i1.setItemName("アイテム1");
+		itemRepository.saveAndFlush(i1);
+		Item i2 = new Item();
+		i2.setItemName("アイテム2");
+		itemRepository.saveAndFlush(i2);
+		
+		
+		Room r1 = new Room();
+		r1.setName("いっかい");
+		r1.setItem(i1);
+		roomRepository.saveAndFlush(r1);
+		Room r2 = new Room();
+		r2.setName("にかい");
+		r2.setItem(i2);
+		roomRepository.saveAndFlush(r2);
+		
+		MyData d1 = new MyData();
+		d1.setName("tuyano");
+		d1.setAge(123);
+		d1.setMail("syoda@tuyano.com");
+		d1.setMemo("090999999");
+		d1.setRoom(r1);
+		repository.saveAndFlush(d1);
+		MyData d2 = new MyData();
+		d2.setName("hanako");
+		d2.setAge(15);
+		d2.setMail("hanako@flower");
+		d2.setMemo("080888888");
+		d2.setRoom(null);
+//		d2.setRoom(r1);
+		repository.saveAndFlush(d2);
+		MyData d3 = new MyData();
+		d3.setName("sachiko");
+		d3.setAge(37);
+		d3.setMail("sachico@happy");
+		d3.setMemo("070777777");
+		d3.setRoom(r2);
+		repository.saveAndFlush(d3);
 	}
 
 
