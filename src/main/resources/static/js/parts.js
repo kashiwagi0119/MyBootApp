7e4d7f1f399c2a756357e4fd482dbc8a92d0a306
JR90077 <JR90077@MPC000025642.mhi.co.jp>
2018-07-06 11:19:08 +0900

    確認メッセージを追加

--- src/main/resources/static/js/parts.js
+++ src/main/resources/static/js/parts.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
     $("#alertbtn").click(function() {
-    	alertify.alert("This is an alert dialog.");  	  
+    	alertify.alert('メッセージタイトル', 'メッセージの内容');
     });
+    $("#confirmbtn").click(function() {
+    	alertify.confirm('確認メッセージ', '実行しますか？'
+    	, function(){ alertify.success('Ok') }
+        , function(){ alertify.error('Cancel')});
+    });
+    
+    
 });
 
