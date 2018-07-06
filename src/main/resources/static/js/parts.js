d146efbb0402ef8c40ba329750ca325faa04edd0
JR90077 <JR90077@MPC000025642.mhi.co.jp>
2018-07-06 17:30:06 +0900

    インプットボックス

--- src/main/resources/static/js/parts.js
+++ src/main/resources/static/js/parts.js
@@ -4,8 +4,21 @@ $(document).ready(function(){
     });
     $("#confirmbtn").click(function() {
     	alertify.confirm('確認メッセージ', '実行しますか？'
-    	, function(){ alertify.success('Ok') }
-        , function(){ alertify.error('Cancel')});
+    	, function(){ 
+    		alertify.success('Ok') 
+    	}
+        , function(){ 
+        	alertify.error('Cancel')
+        });
+    });
+    $("#inputbtn").click(function() {
+    	alertify.prompt("入力", "名前を入力してください。", "デフォルト文字列"
+    	,　function(evt, value){
+    		alertify.success('Ok: ' + value);
+    	}
+    	,　function(){
+    		alertify.error('Cancel');
+    	});
     });
     
     
