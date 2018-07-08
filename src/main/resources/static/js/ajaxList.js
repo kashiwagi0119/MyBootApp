f6d447c555d54423e9e90b5e20f52131ece3cc22
kashi <kashi@com>
2018-07-08 16:49:15 +0900

    グリッドの高さをCSSで

--- src/main/resources/static/js/ajaxList.js
+++ src/main/resources/static/js/ajaxList.js
@@ -38,8 +38,6 @@ function displayGrid(data) {
             ],
     });
     // グリッドの高さ
-//    $('#grid').parents('div.ui-jqgrid-bdiv').css('min-height', '330px');
-    $('#grid').parents('div.ui-jqgrid-bdiv').css('max-height', '330px');
-    
+
 }
 
