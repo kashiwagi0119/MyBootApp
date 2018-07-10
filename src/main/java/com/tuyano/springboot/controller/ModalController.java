eead32f02d69a7dff3c3c7aa063c00eac30e4f13
JR90077 <JR90077@MPC000025642.mhi.co.jp>
2018-07-10 17:34:11 +0900

    モーダル追加

---
+++ src/main/java/com/tuyano/springboot/controller/ModalController.java
@@ -0,0 +1,37 @@
+package com.tuyano.springboot.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.tuyano.springboot.MyDataForm;
+import com.tuyano.springboot.constant.Check2Enum;
+import com.tuyano.springboot.constant.Radio1Enum;
+import com.tuyano.springboot.constant.SelectEnum;
+import com.tuyano.springboot.entity.Item;
+import com.tuyano.springboot.entity.MyData;
+import com.tuyano.springboot.entity.Room;
+import com.tuyano.springboot.repositories.ItemRepository;
+import com.tuyano.springboot.repositories.MyDataRepository;
+import com.tuyano.springboot.repositories.RoomRepository;
+import com.tuyano.springboot.service.MyDataService;
+
+@Controller
+@SessionAttributes(value = "myDataForm")
+public class ModalController {
+	  
+	@RequestMapping(value = "/Modal")
+	public String parts(Model model) {
+		return "modal/modal";
+	}
+
+}
