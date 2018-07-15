b76b9fe92cfeb1fd2b29c1e1a1e87fcb1244da1a
kashi <kashi@com>
2018-07-15 21:27:26 +0900

    MYSQL前に戻す

--- src/main/java/com/tuyano/springboot/MyBootAppApplication.java
+++ src/main/java/com/tuyano/springboot/MyBootAppApplication.java
@@ -20,40 +20,40 @@ public class MyBootAppApplication {
 		SpringApplication.run(MyBootAppApplication.class, args);
 	}
 	
-   @Bean
-   public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-      LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-      em.setDataSource(dataSource());
-      em.setPackagesToScan(new String[] {"com.tuyano.springboot"});
-
-      HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-      vendorAdapter.setDatabase(Database.MYSQL);
-      vendorAdapter.setShowSql(true);
-      em.setJpaVendorAdapter(vendorAdapter);
-      
-      Map<String, Object> properties = new HashMap<>();
-//      properties.put("hibernate.ejb.naming_strategy", "org.springframework.boot.orm.jpa.hibernate.SpringNamingStrategy");
-//      properties.put("hibernate.ejb.naming_strategy", "SuffixNamingStrategy");
-//      properties.put("spring.jpa.hibernate.naming.physical-strategy", "org.hibernate.boot.model.naming.PhysicalNamingStrategyStandardImpl");
-//      properties.put("spring.jpa.hibernate.naming.implicit-strategy", "org.hibernate.boot.model.naming.ImplicitNamingStrategyLegacyJpaImpl");
-//      properties.put("hibernate.ejb.naming_strategy", "org.hibernate.cfg.ImprovedNamingStrategy");
-      properties.put("spring.jpa.hibernate.naming.physical-strategy", "TestNamingStrategy");
-//      properties.put("hibernate.ejb.naming_strategy", "org.hibe1rnate.cfg.EJB3NamingStrategy");
-      em.setJpaPropertyMap(properties);
-      em.afterPropertiesSet();
-     
-      
-      return em;
-   }
- 
-   @Bean
-   public DataSource dataSource(){
-      DriverManagerDataSource dataSource = new DriverManagerDataSource();
-      dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-	  dataSource.setUrl("jdbc:mysql://localhost:3306/taka1?characterEncoding=UTF-8");
-	  dataSource.setUsername("root");
-	  dataSource.setPassword("0422");
-      return dataSource;
-   }
+//   @Bean
+//   public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+//      LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+//      em.setDataSource(dataSource());
+//      em.setPackagesToScan(new String[] {"com.tuyano.springboot"});
+//
+//      HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+//      vendorAdapter.setDatabase(Database.MYSQL);
+//      vendorAdapter.setShowSql(true);
+//      em.setJpaVendorAdapter(vendorAdapter);
+//      
+//      Map<String, Object> properties = new HashMap<>();
+////      properties.put("hibernate.ejb.naming_strategy", "org.springframework.boot.orm.jpa.hibernate.SpringNamingStrategy");
+////      properties.put("hibernate.ejb.naming_strategy", "SuffixNamingStrategy");
+////      properties.put("spring.jpa.hibernate.naming.physical-strategy", "org.hibernate.boot.model.naming.PhysicalNamingStrategyStandardImpl");
+////      properties.put("spring.jpa.hibernate.naming.implicit-strategy", "org.hibernate.boot.model.naming.ImplicitNamingStrategyLegacyJpaImpl");
+////      properties.put("hibernate.ejb.naming_strategy", "org.hibernate.cfg.ImprovedNamingStrategy");
+//      properties.put("spring.jpa.hibernate.naming.physical-strategy", "TestNamingStrategy");
+////      properties.put("hibernate.ejb.naming_strategy", "org.hibe1rnate.cfg.EJB3NamingStrategy");
+//      em.setJpaPropertyMap(properties);
+//      em.afterPropertiesSet();
+//     
+//      
+//      return em;
+//   }
+// 
+//   @Bean
+//   public DataSource dataSource(){
+//      DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//      dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+//	  dataSource.setUrl("jdbc:mysql://localhost:3306/taka1?characterEncoding=UTF-8");
+//	  dataSource.setUsername("root");
+//	  dataSource.setPassword("0422");
+//      return dataSource;
+//   }
 	   
 }
