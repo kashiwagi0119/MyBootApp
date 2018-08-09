5622bd2575902d9335d092344199ad015bb1104f
kashi <kashi@com>
2018-08-09 21:49:06 +0900

    ファイルをフルパスに

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
   // ファイル選択
   $('.custom-file-input').on('change',function(){
-    $(this).next('.custom-file-label').html($(this)[0].files[0].name);
+	  $(this).next('.custom-file-label').html($(this)[0].value);
+//	  var filepath = $(this)[0].value;
+//	  filepath = filepath.replace('\','¥');
+//    $(this).next('.custom-file-label').html(filepath);
+//    $(this).next('.custom-file-label').html($(this)[0].files[0].name);
+    var tmppath = URL.createObjectURL($(this)[0].files[0]);
+    var bbb = $('#customFile')[0].files[0].name
+//    var fName  = document.form.customFile.value;
   });
   // ツールチップ
   $('[data-toggle="tooltip"]').tooltip();
