5d09ac3503f9e12fee471d1fc51ee57a09d1f58a
JR90077 <JR90077@MPC000025642.mhi.co.jp>
2018-12-11 16:56:48 +0900

    common.js

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,30 +1,3 @@
-$(document).ready(function(){
-  // ファイル選択
-  $('.custom-file-input').on('change',function(){
-	  $(this).next('.custom-file-label').html($(this)[0].value);
-//	  var filepath = $(this)[0].value;
-//	  filepath = filepath.replace('\','¥');
-//    $(this).next('.custom-file-label').html(filepath);
-//    $(this).next('.custom-file-label').html($(this)[0].files[0].name);
-    var tmppath = URL.createObjectURL($(this)[0].files[0]);
-    var bbb = $('#customFile')[0].files[0].name
-//    var fName  = document.form.customFile.value;
-  });
-  // ツールチップ
-  $('[data-toggle="tooltip"]').tooltip();
-  // カレンダー
-  $(function () {
-    $('.date').datepicker({
-      language: 'ja',
-      format: 'yymmdd',
-      autoclose: true,
-      todayHighlight : true
-    });
-	// 終了
-	$('#bbb').text(new Date());
-  });
-});
-
 function alertSystemError() {
 	alertify.alert('システムエラー', 'システムエラーが発生しました。');
 }
@@ -32,20 +5,107 @@ function alertSystemError(err) {
 	alertify.alert('システムエラー', 'システムエラーが発生しました。<br/>' + err.responseJSON);
 }
 
+
+$(document).ready(function() {
+
+	// ファイル選択
+	$('.custom-file-input').on('change', function() {
+		// フルパス
+		var result = $(this)[0].value.replace( /\\/g, '&yen;' );
+		$(this).next('.custom-file-label').html(result);
+		// ファイル名のみ
+		// $(this).next('.custom-file-label').html($(this)[0].files[0].name);
+	});
+
+	// ツールチップ
+	$('[data-toggle="tooltip"]').tooltip();
+
+	// カレンダー（yyyy/mm/dd）
+	$(function() {
+		$('.dateyyyymmdd').datepicker({
+			language : 'ja',
+			format : 'yyyy/mm/dd',
+			autoclose : true,
+			todayHighlight : true,  // 当日をハイライトする
+			forceParse : false,  // 入力値の解析をしない
+			showOnFocus : false,  // 入力フィールドフォーカス時にカレンダー表示しない
+		});
+	});
+
+	// カレンダー（yymmdd）
+	$(function() {
+		$('.dateyymmdd').datepicker({
+			language : 'ja',
+			format : 'yymmdd',
+			autoclose : true,
+			todayHighlight : true,  // 当日をハイライトする
+			forceParse : false,  // 入力値の解析をしない
+			showOnFocus : false,  // 入力フィールドフォーカス時にカレンダー表示しない
+		});
+	});
+
+	// モーダル画面を移動可能にする
+	$(document).ready(function(){
+		$(".modal").draggable({
+		    handle: ".modal-header"
+		});
+	});
+
+});
+
+function alertSystemError() {
+	formSubmit('/sendErrorPage');
+	//alert('システムエラーが発生しました。');
+}
+
 /**
- * エラー確認 メッセージ表示
- * エラーの場合、trueを返す
+ * エラー確認 メッセージ表示 エラーの場合、trueを返す
  */
 function isJsonError(json) {
-	
+
 	if (json.errorList.length > 0) {
-		alertify.alert('メッセージ', json.errorList.join('<br/>'));
+		alert(json.errorList.join('\n'));
 		return true;
 	}
 	if (json.messageList.length > 0) {
-		alertify.alert('メッセージ', json.messageList.join('<br/>'));
+		alert(json.messageList.join('\n'));
 	}
 	return false;
 }
+
+/**
+ * 画面ロック
+ */
+function lockScreen() {
+	$.blockUI({
+		message : '処理中です。お待ちください。',
+		css: {'z-index': 99999999},
+		overlayCSS: {'z-index': 99999999},
+	});
+}
+
+/**
+ * 画面ロック解除
+ */
+function unLockScreen() {
+	$.unblockUI();
+}
+
+/**
+ * フォームサブミット
+ */
+function formSubmit(url) {
+	// 元のアクションを退避
+	var beforeAction = $('#form').attr('action');
+
+    $('#form').attr('action', '/' + applicationCode +  url);
+    $('#form').submit();
+
+    // アクションを戻しておく
+    $('#form').attr('action', beforeAction);
+    return false;
+}
+
+
 
 
