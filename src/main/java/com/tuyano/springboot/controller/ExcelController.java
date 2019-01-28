34ad6b040fa0f9b6a1edf500d0780f2582908998
JR90077 <JR90077@MPC000025642.mhi.co.jp>
2019-01-28 17:00:59 +0900

    EXCELテンプレートダウンロード

--- src/main/java/com/tuyano/springboot/controller/ExcelController.java
+++ src/main/java/com/tuyano/springboot/controller/ExcelController.java
@@ -1,27 +1,30 @@
 package com.tuyano.springboot.controller;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
-//import javax.inject.Inject;
-
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tuyano.springboot.dto.Message;
-import com.tuyano.springboot.dto.Reserve;
 import com.tuyano.springboot.view.MessagesXlsxView;
 
 @Controller
 public class ExcelController {
-	  
+
+    @Autowired
+    ResourceLoader resourceLoader;
+
 	@RequestMapping(value = "/excel")
 	public String index() {
 		return "/excel";
@@ -39,48 +42,24 @@ public class ExcelController {
 		}
         return new ModelAndView(new MessagesXlsxView(), "messages", messages);
     }
-    
-    // Excelダウンロードテンプレート
-    /**
-     * [dummy] 帳票データを読み込むためのobjectMapper
-     */
-//    @Inject
-    ObjectMapper objectMapper;
 
     /**
-     * [dummy] 帳票データを格納したjsonファイル
+     * テンプレートのダウンロード
+     * @return
+     * @throws UnsupportedEncodingException
      */
-    @Value("${app.sample.jsonFile:C:/temp/jsonData.json}")
-    private File jsonDataFile;
-
-    @RequestMapping(value = "/ExcelDownloadTemplate")
-    public String excelDownloadTemplate(Model model) {
-
-        // 1. [dummy] get report data
-        Reserve reserve = this.findReserve();
-        // 2. set report data to model
-        model.addAttribute(reserve);
-        model.addAttribute("fileName", "予約明細.xlsx");
-        // 3. return excel view bean's name
-        return "reservationExcelView";
+    @RequestMapping("/ExcelDownloadTemplate")
+    public ResponseEntity<Resource> c002FileDownload() throws UnsupportedEncodingException {
+    	String filepath = "static/excelTemplate/C002.xlsx";
+    	Resource resource = resourceLoader.getResource("classpath:" + filepath);
+    	HttpHeaders headers = new HttpHeaders();
+    	headers.add("Content-Type", "application/octet-stream");
+    	headers.add("Content-Disposition", "attachment; filename*=UTF-8''" + URLEncoder.encode("テンプレートEXCELファイル名.xlsx", StandardCharsets.UTF_8.name()));
+        HttpStatus status = HttpStatus.OK;
+        return new ResponseEntity<>(resource, headers, status);
     }
 
-    /**
-     * [dummy] 帳票に出力するためのダミーの予約情報を作成する
-     * @return 予約
-     */
-    private Reserve findReserve() {
-        Reserve reserve = null;
-        try {
-            reserve = objectMapper.readValue(jsonDataFile, Reserve.class);
-            System.out.println(objectMapper.writeValueAsString(reserve));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return reserve;
-    }
-    
 
-    
-    
+
+
 }
